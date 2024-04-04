b27cb30416dc4ada74c98c8bed6aee7b1b4bbe78
Aqib1422 <mraqibshafique@gmail.com>
2024-04-05 02:35:15 +0500

    main4

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let arrowicon=document.querySelector('.arrowdown-icon')
 let langlist=document.querySelector('.lang-list')
 let langli=document.querySelectorAll('.lang-li')
 let EnglishText=document.querySelector('.eng-text')
+let menulinesdiv=document.querySelector('.menu-lines-div')
+let menutext=document.querySelector('.menu-text')
+let menulines=document.querySelectorAll('.menu-line')
 
 arrowicon.addEventListener('click',()=>
 {
@@ -14,4 +17,18 @@ langli.forEach((languges)=>
     languges.addEventListener('click',()=>{
         EnglishText.innerHTML=languges.innerHTML
     })
+})
+menulinesdiv.addEventListener('click',()=>
+{
+    if(menutext.innerHTML=='Menu')
+    {
+        menutext.innerHTML='Close'
+    }
+    else
+    {
+        menutext.innerHTML='Menu'
+    }
+ menulines[0].classList.toggle('menu-line1-animation')
+ menulines[1].classList.toggle('menu-line2-animation')
+ menulines[2].classList.toggle('menu-line3-animation')
 })
