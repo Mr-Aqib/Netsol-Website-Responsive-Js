503b16072b61edc762f76fc03b84a75f00f629fe
Aqib1422 <mraqibshafique@gmail.com>
2024-04-06 22:06:16 +0500

    menu-items2

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-let arrowicon=document.querySelector('.arrowdown-icon')
-let langlist=document.querySelector('.lang-list')
-let langli=document.querySelectorAll('.lang-li')
-let EnglishText=document.querySelector('.eng-text')
-let menulinesdiv=document.querySelector('.menu-lines-div')
-let menutext=document.querySelector('.menu-text')
-let menulines=document.querySelectorAll('.menu-line')
-
-arrowicon.addEventListener('click',()=>
-{
+let arrowicon = document.querySelector('.arrowdown-icon')
+let langlist = document.querySelector('.lang-list')
+let langli = document.querySelectorAll('.lang-li')
+let EnglishText = document.querySelector('.eng-text')
+let menulinesdiv = document.querySelector('.menu-lines-div')
+let menutext = document.querySelector('.menu-text')
+let menulines = document.querySelectorAll('.menu-line')
+let menuitems = document.querySelectorAll('.menu-items')
+let menuitemitems = document.querySelectorAll('.menu-item-items')
+let menuitemitems1 = document.querySelector('.menu-item-items1')
+let sidebar = document.querySelector('.sidebar')
+arrowicon.addEventListener('click', () => {
     arrowicon.classList.toggle('arrow-icon-animation')
     langlist.classList.toggle('lang-list-animation')
+    
 
 })
-langli.forEach((languges)=>
-{
-    languges.addEventListener('click',()=>{
-        EnglishText.innerHTML=languges.innerHTML
+langli.forEach((languges) => {
+    languges.addEventListener('click', () => {
+        EnglishText.innerHTML = languges.innerHTML
     })
 })
-menulinesdiv.addEventListener('click',()=>
-{
-    if(menutext.innerHTML=='Close')
-    {
-        menutext.innerHTML='Menu'
+menulinesdiv.addEventListener('click', () => {
+    if (menutext.innerHTML == 'Close') {
+        menutext.innerHTML = 'Menu'
+    } else {
+        menutext.innerHTML = 'Close'
     }
-    else
-    {
-        menutext.innerHTML='Close'
-    }
- menulines[0].classList.toggle('menu-line1-animation')
- menulines[1].classList.toggle('menu-line2-animation')
- menulines[2].classList.toggle('menu-line3-animation')
+    menulines[0].classList.toggle('menu-line1-animation')
+    menulines[1].classList.toggle('menu-line2-animation')
+    menulines[2].classList.toggle('menu-line3-animation')
+    sidebar.classList.toggle('sidebar-animation')
+})
+
+
+
+menuitems.forEach((menu) => {
+    menu.addEventListener('click', () => {
+
+        menuitemitems1.classList.toggle('menu-item-ani')
+    })
 })
